4a7e762a22c208f73b604e2efbe3fed5476952c1
Simon He <13917107469@163.com>
2023-03-21 12:13:30 +0800

    chore: add button style

--- playground/src/pages/index.vue
+++ playground/src/pages/index.vue
@@ -1,8 +1,36 @@
 <script setup lang="ts">
 import { registerButton } from '../../../src/button'
 registerButton()
+
 </script>
 
 <template>
-  <adw-button>你好</adw-button>
+  <div flex="~ gap-2" py4>
+    <adw-button type="default" >default</adw-button>
+    <adw-button type="default" danger>default danger</adw-button>
+    <adw-button type="default" disabled>default disabled</adw-button>
+  </div>
+  <div flex="~ gap-2" py4>
+    <adw-button type="primary">primary</adw-button>
+  <adw-button type="primary" danger>primary danger</adw-button>
+  <adw-button type="primary" disabled>primary disabled</adw-button>
+</div>
+
+<div flex="~ gap-2" py4>
+  <adw-button type="dashed">dashed</adw-button>
+  <adw-button type="dashed" danger>dashed danger</adw-button>
+  <adw-button type="dashed" disabled>dashed disabled</adw-button>
+</div>
+
+<div flex="~ gap-2" py4>
+  <adw-button type="link">link</adw-button>
+  <adw-button type="link" danger>link danger</adw-button>
+  <adw-button type="link" disabled>link disabled</adw-button>
+</div>
+<div flex="~ gap-2" py4>
+  <adw-button type="text">text</adw-button>
+  <adw-button type="text" danger>text danger</adw-button>
+  <adw-button type="text" disabled>text disabled</adw-button>
+</div>
+
 </template>
--- src/button.ts
+++ src/button.ts
@@ -33,13 +33,84 @@ export class AdwButton extends BaseWebComponent {
           color: #4096ff;
           border-color: #4096ff;
         }
+
+        .${this.prefix}-primary {
+          color: #fff;
+          background-color: #1677ff;
+          box-shadow: 0 2px 0 rgba(5,145,255,.1);
+        }
+        .${this.prefix}-primary:hover {
+          background-color: #4096ff;
+        }
+
+        .${this.prefix}-dashed {
+          background-color: #fff;
+          border-color: #d9d9d9;
+          box-shadow: 0 2px 0 rgba(0,0,0,.02);
+          border-style: dashed;
+        }
+        .${this.prefix}-dashed:hover {
+          color: #4096ff;
+          border-color: #4096ff;
+        }
+
+        .${this.prefix}-text {
+
+        }
+        .${this.prefix}-text:hover {
+          background-color: rgba(0,0,0,.06);
+        }
+
+        .${this.prefix}-link {
+          color: #1677ff;
+        }
+        .${this.prefix}-link:hover {
+          color: #69b1ff;
+        }
+
+        .${this.prefix}[danger] {
+          color: #ff4d4f;
+        }
+        .${this.prefix}-primary[danger],.${this.prefix}-default[danger],.${this.prefix}-dashed[danger] {
+          border-color: #ff4d4f;
+          background-color:transparent;
+        }
+        .${this.prefix}-primary[danger],.${this.prefix}-default[danger],.${this.prefix}-dashed[danger] {
+          border-color: #ffa39e;
+        }
+        .${this.prefix}[danger]:hover {
+          color: #ff7875;
+        }
+        .${this.prefix}-text[danger]:hover {
+          color: #ff7875;
+          background-color: #fff2f0;
+        }
+
+        .${this.prefix}[disabled]{
+          cursor: not-allowed;
+          color: rgba(0,0,0,.25);
+          background-color: transparent;
+        }
+        .${this.prefix}-primary[disabled],.${this.prefix}-default[disabled],.${this.prefix}-dashed[disabled]{
+          border-color: #d9d9d9;
+          background-color: rgba(0,0,0,.04);
+          box-shadow: none;
+        }
         `
   }
 
   template(): string {
-    const { type = 'default' } = this.props
-    const btnClass = `${this.prefix}-${type}`
-    return `<button class="${btnClass}"><slot></slot></button>`
+    const { type = 'default',disabled } = this.props
+    console.log(this.props);
+    const attributes = Object.keys(this.props).map(key=>{
+      const value = this.props[key]
+      if(value ==='')
+        return key
+      return `${key}="${value}"`
+    }).join(' ')
+
+    const btnClass = `${this.prefix} ${this.prefix}-${type}`
+    return `<button class="${btnClass}" ${attributes}><slot></slot></button>`
   }
 }
 
